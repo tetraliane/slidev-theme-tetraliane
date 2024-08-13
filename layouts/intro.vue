aec1ff4bebba8786efbc2131ab47c227a58c248c
R Watanabe <57061591+tetraliane@users.noreply.github.com>
2024-08-14 00:41:23 +0900

    feat: impl intro layout

--- layouts/intro.vue
+++ layouts/intro.vue
@@ -1,7 +1,29 @@
 <template>
-  <div class="slidev-layout intro">
-    <div class="my-auto">
+  <div class="tetraliane-layout intro">
+    <div>
       <slot />
+    </div>
+    <div class="info">
+      <slot name="info" />
     </div>
   </div>
 </template>
+
+<style scoped>
+.intro {
+  @apply h-full grid;
+  grid-template-rows: repeat(2, 1fr);
+}
+
+.intro > :first-child {
+  @apply self-end;
+}
+
+.intro :deep(h1) {
+  @apply text-6xl mb-0;
+}
+
+.info :deep(p) {
+  @apply opacity-50;
+}
+</style>
--- styles/index.ts
+++ styles/index.ts
@@ -1,5 +1,3 @@
-// inherit from base layouts, remove it to get full customizations
-import "@slidev/client/styles/layouts-base.css";
-import "./layout.css";
+import "./global.css";
 // TODO: remove this in v0.50
 import "./prism.css";
